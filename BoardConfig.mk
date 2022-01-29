ace2dbbe8b38e060decc98bb3f1f0283cf948d79
Kunmun <kunmun.devroms@gmail.com>
2022-01-29 21:56:53 +0530

    X01BD: Commonize DT2W node
    This reverts commit 2eede764c0540ddf5346bf5ba25da8bb5aecdf67.
    
    Change-Id: I4b0544ff4697ba994c8f3fd01854b6625a396f58

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,5 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := asus/X01BD_defconfig
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
-
 # Inherit the proprietary files
 include vendor/asus/X01BD/BoardConfigVendor.mk
